3e1a03e675d27dc9505ef68e56161c8d9009f7aa
akmaladnann <112589837+akmaladnann@users.noreply.github.com>
2023-04-13 11:56:40 +0700

    Add files via upload

---
+++ Pra_utp_Petruk.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+//int penjumlahan(int a, int b)
+//{
+//	return a+b;
+//}
+
+//void absen(string nama, long int npm)
+//{
+//	cout<<"Nama : "<<nama<<"\n";
+//	cout<<"NPM : "<<npm;
+//}
+
+int main()
+{
+//	int a,b,hasil;
+//	cin>>a>>b;
+//	hasil = penjumlahan(a,b);
+//	cout<<hasil;
+
+//	string nama;
+//	long int npm;
+//	cout<<"Nama : ";
+//	cin>>nama;
+//	cout<<"NPM : ";
+//	cin>>npm;
+//	cout<<"Nama saya adalah "<<nama<<" dengan NPM "<<npm;
+
+//	int arr[5] = {1,2,3,4,5};
+//	for (int i=0; i<5; i++)
+//	{
+//		cout<<"Nilai ke "<<i<<" adalah "<<arr[i]<<"\n";
+//	}
+
+//	int i,j;
+//	int arr[3][4];
+//	for (i=0; i<3; i++)
+//	{
+//		for (j=0; j<4; j++)
+//		{
+//			arr[i][j] = i*j;
+//			cout<<arr[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+
+//	int a;
+//	cin>>a;
+//	int* ptr = &a;
+//	cout<<*ptr<<" "<<ptr;
+//	//*ptr = nilai nya, ptr aja = lokasi nya
+
+//	int arr[5] = {1,2,3,4,5};
+//	int* ptr = arr;
+//	
+//	for (int i=0; i<5; i++)
+//	{
+//		cout<<"Nilai array ke "<<i<<" adalah "<<*ptr<<endl;
+//		ptr++;
+//	}
+
+//	ofstream buat_file("buat.txt");
+//	buat_file<<"halo";
+//	buat_file.close();
+//
+//	string teks;
+//	ifstream baca_file("buat.txt");
+//	while(getline(baca_file,teks))
+//	{
+//		cout<<teks;	
+//	}
+
+//	string teks[4];
+//	
+//	for (int i=0; i<4; i++)
+//	{
+//		cin>>teks[i];
+//	}
+//	ofstream buatt_file("tes.txt");
+//	
+//	for (int i=0; i<4; i++)
+//	{
+//		buatt_file<<teks[i]<<"\n";
+//	}
+//	
+//	string t;
+//	ifstream bacaa_file("tes.txt");
+//	while (getline(bacaa_file,t))
+//	{
+//		cout<<t<<"\n";
+//	}
+
+}
